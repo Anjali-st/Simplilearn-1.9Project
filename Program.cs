bcaba43b86f0ba47637e67d630b870c0e7ab839c
Anjali-st <anjali90908080@gmail.com>
2023-07-25 21:22:47 +0530

    1.9_Project

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppProject_1._9
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                string filePath = @"D:\DotNet\Project_1.9\ConAppProject-1.9\Student.txt";
+                try
+                {
+
+                    Console.WriteLine("Student Data:");
+                    Console.WriteLine("-------------");
+                    string fileContent = File.ReadAllText(filePath);
+                    Console.WriteLine(fileContent);
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+                finally
+                {
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
